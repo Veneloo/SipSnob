c54db81abb490988e916bc6a1f551dd500fa23c3
laurensgalarza <92804326+laurensgalarza@users.noreply.github.com>
2025-03-02 14:53:45 -0500

    working on startup screen

--- sipsnob/src/App.jsx
+++ sipsnob/src/App.jsx
@@ -1,20 +1,37 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
+import { BrowserRouter, Route, Link, Routes } from 'react-router-dom';
 import HomePage from './pages/home'
+import LogIn from './pages/login';
+import SignUp from './pages/signup';
+
+function SignIn(){
+
+
+}
 
 function App() {
   return (
       <div>
       <h1> SipSnob </h1>
-      <h2> Log In 
+    <BrowserRouter>
+       
+    
+      <nav>
+      <Link to="/pages/login">Login</Link>
+      <Link to="/pages/signup">Signup</Link>
 
-      </h2>
+      </nav>
+      <Routes>
+        <Route path="/" element={<h2> Log In </h2>}/>
 
-      <h2> Don't have an account? </h2>
-      </div>
+
+      </Routes>
+      <h2>  Sign Up</h2>
+    </BrowserRouter> 
+     </div>
 
   )
 }
