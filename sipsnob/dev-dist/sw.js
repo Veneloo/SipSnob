a1738ef22180b9c3834fff339a47c714306fafe0
Veneloo <axel.cazorla10@gmail.com>
2025-04-20 15:29:14 -0400

    Updated Discover to Fetch API calls

--- sipsnob/dev-dist/sw.js
+++ sipsnob/dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.papqavn69mo"
+    "revision": "0.m0ii9h97u9"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
