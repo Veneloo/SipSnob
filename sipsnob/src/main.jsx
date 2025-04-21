626e0b51d5c1a995f337eb182d53569e07d0ca7d
hcxra <hannahccera@gmail.com>
2025-04-20 20:34:48 -0400

    fixed deployment

--- sipsnob/src/main.jsx
+++ sipsnob/src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
 
-import { registerSW } from "virtual:pwa-register";
-import { AuthProvider } from "./context/authContext";
+import { registerSW } from 'virtual:pwa-register';
+import { AuthProvider } from './context/authContext';
 
 registerSW();
 
