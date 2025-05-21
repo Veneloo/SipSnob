d58c8dd2e184bfd49c50ad4379a11ac868c09f86
LilianaMasoud <liliana.masoud89@myhunter.cuny.edu>
2025-05-20 21:06:17 -0400

    pushing test

--- sipsnob/dev-dist/sw.js
+++ sipsnob/dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.t8ectprdju"
+    "revision": "0.bp70kvil7i8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
