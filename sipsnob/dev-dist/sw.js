9ee2766a5b639695c610e9bd8d318d5b645fd6ee
Veneloo <axel.cazorla10@gmail.com>
2025-05-20 21:46:56 -0400

    update

--- sipsnob/dev-dist/sw.js
+++ sipsnob/dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.bp70kvil7i8"
+    "revision": "0.gmn5vt23tag"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
